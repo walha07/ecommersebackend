5dc9bfa24770a82500758e9af311d9a343c0c813
Your Name <Your Name>
2023-06-21 07:14:31 -1100

    first commit

---
+++ routes/article.route.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const Article=require("../models/article")
+// afficher la liste des article.
+router.get('/', async (req, res, )=> {
+    try {
+    const art = await Article.find();
+    
+    res.status(200).json(cat);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+    });
+
+// créer un nouvelle article
+router.post('/', async (req, res) => {
+    const { reference, designation,prix,marque,qtestock,imageart,scategorieID} = req.body;
+    const newCategorie = new Categorie({reference:reference,designation:designation,
+        prix:prix,marque:marque,qtestock:qtestock,imageart:imageart,scategorieID:scategorieID})
+    
+    try {
+    await newArticle.save();
+    
+    res.status(200).json(newArticle );
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+    });
+
+// chercher un article
+router.get('/:reference',async(req, res)=>{
+    try {
+    const art = await Article.findById(req.params.categorieId);
+    res.status(200).json(cat);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+// modifier une article
+router.put('/:reference', async (req, res)=> {
+    const { reference, designation,prix,marque,qtestock,imageart,scategorieID} = req.body;
+    const ref = req.params.reference;
+    try {
+    const art1 = {
+        reference:reference,designation:designation,
+        prix:prix,marque:marque,qtestock:qtestock,imageart:imageart,scategorieID:scategorieID };
+    await Article.findByIdAndUpdate(ref, art1);
+    res.json(art1);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+
+// Supprimer un article
+router.delete('/:reference', async (req, res)=> {
+    const id = req.params.reference;
+    await Article.findByIdAndDelete(ref);
+    
+    res.json({ message: "article deleted successfully." });
+    });
+    module.exports = router;
+    
---
+++ routes/scategorie.route.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const Scategorie=require("../models/scategorie")
+// afficher la liste des scategories.
+router.get('/', async (req, res, )=> {
+    try {
+    const scat = await Scategorie.find();
+    
+    res.status(200).json(cat);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+    });
+    // créer un nouvelle scatégorie
+router.post('/', async (req, res) => {
+    const { nomscategorie, imagescat,categorieID} = req.body;
+    const newScategorie = new Scategorie({nomscategorie:nomscategorie,
+    imagescat:imagescat,categorieID:categorieID})
+    
+    try {
+    await newScategorie.save();
+    
+    res.status(200).json(newScategorie );
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+    });
+    // chercher une scatégorie
+router.get('/:scategorieId',async(req, res)=>{
+    try {
+    const scat = await Scategorie.findById(req.params.scategorieId);
+    res.status(200).json(scat);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+    // modifier une scatégorie
+router.put('/:scategorieId', async (req, res)=> {
+    const { nomscategorie, imagescategorie,categorieID} = req.body;
+    const id = req.params.scategorieId;
+    try {
+    const scat1 = {
+    nomscategorie:nomscategorie,imagescat:imagescat,categorieID:categorieID, _id:id };
+    await Scategorie.findByIdAndUpdate(id, scat1);
+    res.json(scat1);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+    // Supprimer une scatégorie
+router.delete('/:scategorieId', async (req, res)=> {
+    const id = req.params.scategorieId;
+    await Scategorie.findByIdAndDelete(id);
+    
+    res.json({ message: "scategorie deleted successfully." });
+    });
+    module.exports = router;
